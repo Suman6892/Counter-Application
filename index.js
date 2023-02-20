4c73a38a09644edf507abfc29e0af45c774e254d
Suman Kumar Mondal <124084455+Suman6892@users.noreply.github.com>
2023-02-20 21:13:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import {Component} from 'react'
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+  onIncrement = () => {
+    this.setState(prevState => {
+      return {count: prevState.count + 1}
+    })
+  }
+  onDecrement = () => {
+    this.setState(prevState => {
+      return {count: prevState.count - 1}
+    })
+  }
+  onReset = () => {
+    this.setState(prevState => {
+      return {count: 0}
+    })
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count">{count}</p>
+        <div>
+          <button onClick={this.onIncrement} className="button">
+            Increase
+          </button>
+          <button onClick={this.onReset} className="button">
+            Reset
+          </button>
+          <button onClick={this.onDecrement} className="button">
+            Decrease
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default Counter
